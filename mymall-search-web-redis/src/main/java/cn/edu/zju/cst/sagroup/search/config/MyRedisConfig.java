b0ab7370498dda5fac821f0f9215287b3745f4f7
maoyiwei <jsmyw95@qq.com>
2019-01-06 20:44:48 +0800

    添加redis搜索结果静态化
    Signed-off-by: maoyiwei <jsmyw95@qq.com>

---
+++ mymall-search-web-redis/src/main/java/cn/edu/zju/cst/sagroup/search/config/MyRedisConfig.java
@@ -0,0 +1,28 @@
+package cn.edu.zju.cst.sagroup.search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class MyRedisConfig {
+
+    /**
+     * 通用的 RedisTemplate
+     * @param lettuceConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(template.getKeySerializer());
+        template.setHashValueSerializer(template.getValueSerializer());
+        return template;
+    }
+}
