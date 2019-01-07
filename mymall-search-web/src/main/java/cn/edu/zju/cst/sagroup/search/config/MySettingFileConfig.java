233dc955fb5a75f3b9fd21602f790d1cea71e280
jahentao <tzhltjh@163.com>
2019-01-08 01:53:01 +0800

    fix，商品详情页缓存测试通过

--- mymall-search-web/src/main/java/cn/edu/zju/cst/sagroup/search/config/MySettingFileConfig.java
+++ mymall-search-web/src/main/java/cn/edu/zju/cst/sagroup/search/config/MySettingFileConfig.java
@@ -6,6 +6,10 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @PropertySource("classpath:conf/conf.properties")
@@ -14,5 +18,21 @@ public class MySettingFileConfig {
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
         return (factory) -> factory.addContextCustomizers(
                 (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
+    }
+    /**
+     * 通用的 RedisTemplate
+     * @param lettuceConnectionFactory
+     * @return
+     */
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(template.getKeySerializer());
+        template.setHashValueSerializer(template.getValueSerializer());
+        return template;
     }
 }
